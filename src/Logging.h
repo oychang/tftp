be320671dbe11a892b8dd2b46e2eae01e7ede608
Oliver Chang <oliver@oychang.com>
2014-03-05 16:17:04 -0500

    Add function prefix to logger.

--- src/Logging.h
+++ src/Logging.h
@@ -1,9 +1,12 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
+// Macro that depends on a global variable to be set to true/falsey
+// and prints to stdout with normal printf formatting information.
 #include <stdio.h>
 static int VERBOSE = 0;
 #define log(format_string, ...) \
+        if (VERBOSE) printf("%s: ", __FUNCTION__); \
         if (VERBOSE) printf(format_string, ##__VA_ARGS__)
 
 #endif
